f25a0d3cc8c9d811d6dde1f1ff5d38f366e247f2
Luke Murray <lmurray.nyc@gmail.com>
2016-07-19 20:07:50 -0400

    second commit

---
+++ hunta/ViewControllerMapFunctions.swift
@@ -0,0 +1,11 @@
+//
+//  ViewControllerMapFunctions.swift
+//  hunta
+//
+//  Created by Luke Murray on 7/19/16.
+//  Copyright © 2016 asleepinthetrees. All rights reserved.
+//
+
+import Foundation
+
+public extension ViewController
